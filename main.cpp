1e27b6f8da2c08ef90bee2a5af71468eec2eaf16
Alice (NosovKV) <alice@example.com>
2024-07-05 17:01:39 +0300

    code: changed int to double

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    double a, b;
     cout << "Enter A and B: ";
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
 << "A - B = " << a - b << '\n';
-    cout << "A * B = " << a * b;
-    cout << "A / B = " << a / b;
+    cout << "A * B = " << a * b <<'\n';
+    cout << "A / B = " << a / b <<'\n';
     if (a > b)
         cout << a << '\n';
     else
